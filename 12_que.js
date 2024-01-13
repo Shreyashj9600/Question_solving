6db2d2ac6e0eb6a532600db5cf24db5f760dcb80
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-13 18:08:59 +0530

    Que.js

---
+++ 12_que.js
@@ -0,0 +1,10 @@
+//Mobile  Validation in JavaScript ?
+
+// mobile 
+
+const num = '0102030402'
+if ( num.length == 10 && !isNaN(num)) {
+        console.log("Valid mobile number");
+} else {
+    console.log("mobile number  not valid");
+}
