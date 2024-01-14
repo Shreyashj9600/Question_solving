9fab8068c309973f02e969c0c7d56d8a31de7d8c
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-14 20:18:33 +0530

    que.js

---
+++ 13_que.js
@@ -0,0 +1,17 @@
+//scope 
+    
+function funOne(){
+    if(2 === 2){
+        var a = 5;
+    }
+    console.log(a);
+}
+funOne()
+
+function funOne(){
+    if(2 === 2){
+        let a = 5;
+    }
+    console.log(a);
+}
+funOne()
