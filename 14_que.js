ad662fb2b20c051c19e64eeaf6838f385e0667f6
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-14 20:52:53 +0530

    que.js

---
+++ 14_que.js
@@ -0,0 +1,13 @@
+//What is Destructuring in Javascript ? 
+
+// Destructuring is a JavaScript expression that allows us to extract data 
+// from arrays, objects, and maps and set them into new, distinct variables.
+// Destructuring allows us to extract multiple properties, or items, from an array​ at a time.
+
+const obj = { name: "shreyash", age: 23, designation: "FrontEnd Developer" }
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.designation);
+
