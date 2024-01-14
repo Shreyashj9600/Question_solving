0029778ddb7f9ef8aedbf90d7892367296bffa0e
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-01-14 11:38:23 +0530

    que.js

--- 12_que.js
+++ 12_que.js
@@ -2,9 +2,16 @@
 
 // mobile 
 
-const num = '0102030402'
+const num = '0102030401'
 if ( num.length == 10 && !isNaN(num)) {
         console.log("Valid mobile number");
 } else {
     console.log("mobile number  not valid");
+}
+
+const email = 'user@example.com' 
+if(email.includes('@') && email.includes('.')){
+    console.log('Email is valid');
+} else {
+    console.log('Email is not valid');
 }
